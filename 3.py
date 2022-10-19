a8bf0ef4762a4f85f076be78427fd1f5dc5b956d
Andrey <megainvest@yandex.ru>
2022-10-19 13:57:39 +0300

    Решение 3 задачи

--- 3.py
+++ 3.py
@@ -20,9 +20,25 @@
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
-my_list = [2, 3, 5, 6]
-lst = []
-for i in my_list:
-    lst = [my_list[0] * my_list[3], my_list[1] * my_list[2]]
-print(lst)    
+# my_list = [2, 3, 5, 6]
+# lst = []
+# for i in my_list:
+    #   lst = [my_list[0] * my_list[3], my_list[1] * my_list[2]]
+# print(lst)    
  
+# Задайте список из вещественных чисел. 
+# Напишите программу, которая найдёт разницу между максимальным 
+# и минимальным значением дробной части элементов.
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+lst = [1.1, 1.2, 3.1, 5, 10.01]
+min = 1
+max = 0
+for i in lst:
+    if (i - int(i)) <= min:
+        min = i - int(i)
+    if (i - int(i)) >= max:
+            max = i - int(i)
+result = max - min
+print(float(round(result, 3)))
