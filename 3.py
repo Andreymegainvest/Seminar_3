73cc7919927255b5ec8ea82c161f5b2b41ceb61c
Andrey <megainvest@yandex.ru>
2022-10-19 19:12:01 +0300

    Конец

--- 3.py
+++ 3.py
@@ -62,23 +62,23 @@
 # для k = 8 список будет выглядеть так:
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-n = int(input('Введите число: '))
-def fibo(n):
-    if n>=0:
-       idx = range(n+1)
-       x = [0,1]
-       for k in idx[2:]:
-           x.append(x[k-1] + x[k-2]) 
-       return x[n]
-    else:
-       n=-(n-1)
-       idx = range(n+1)
-       x = [1,0]
-       for k in idx[2:]:
-           x.append(x[k-2] - x[k-1]) 
-       x.reverse()
-    return x[0]
+# n = int(input('Введите число: '))
+# def fibo(n):
+#     if n>=0:
+#        idx = range(n+1)
+#        x = [0,1]
+#        for k in idx[2:]:
+#            x.append(x[k-1] + x[k-2]) 
+#        return x[n]
+#     else:
+#        n=-(n-1)
+#        idx = range(n+1)
+#        x = [1,0]
+#        for k in idx[2:]:
+#            x.append(x[k-2] - x[k-1]) 
+#        x.reverse()
+#     return x[0]
 
-for i in range(-n,n+1):
-    print(fibo(i), end =' ')
+# for i in range(-n,n+1):
+#     print(fibo(i), end =' ')
 
